901f36a36401504f57c14b2ac36b920aa71c10dd
xfhan <erazer_control@163.com>
2019-12-06 11:32:21 +0800

    添加分数模型

--- score/models.py
+++ score/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from users.models import Student,Subject
+from django.utils import timezone
 # Create your models here.
+
+class Exam(models.Model):
+    name = models.CharField(max_length=30, verbose_name="考试名称")
+
+class Score(models.Model):
+    owner = models.ForeignKey('users.Student',on_delete=models.CASCADE)
+    subject = models.ForeignKey('users.Subject',on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(verbose_name='分值')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='录入时间',db_index=True)
+    is_final = models.BooleanField(default=False,verbose_name="是否期末考试")
+    class Meta:
+       #('add', 'change', 'delete', 'view')
+        default_permissions = []
+        permissions = [
+            ("add_score", "添加成绩"),
+            ("change_score", "修改成绩"),
+            ("delete_score", "删除成绩"),
+            ("view_score", "查看成绩"),
+        ]
--- users/models.py
+++ users/models.py
@@ -4,47 +4,45 @@ from django.utils import timezone
 
 # Create your models here.
 class MyUserManager(BaseUserManager):
-    """
-    The default User Model is too crowd, we use Custom BaseUserManager instead
-    """
-
-    def create_user(self, username, password=None):
+    def create_user(self, username = None, phone = None, password = None):
         """
-        create a user
-        :param username: username of new User
-        :param password: password of new User
-        :return: A BaseUser object
+        Creates and saves a User with the given email, date of
+        birth and password.
         """
+        if not username and not phone:
+            raise ValueError('Users must have an username or phone')
 
         user = self.model(
-            username=username,
+            username = username,
+            phone = phone,
         )
-        user.save(using=self._db)
-        return user
 
-    def create_superuser(self, username, password=None):
-        """
-        create a superuser, this is use for "python manage createsuperuser"
-        :param username: name of new superuser
-        :param password: password of new superuser
-        :return: a BaseUser object with is_admin set to True
-        """
-
-        user = self.create_user(username, password)
-        user.is_admin = True
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, username, phone, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username,
+            phone,
+            password=password,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class BaseUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(verbose_name="身份证号",max_length=18, unique=True, primary_key=True)
     phone = models.CharField(verbose_name="手机号码", unique=True, max_length=15, null=True, blank=True)
-    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = MyUserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ()
+    REQUIRED_FIELDS = ['phone']
 
     class Meta:
         ordering = ('-username',)
@@ -69,20 +67,18 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
 
     @property
     def is_staff(self):
-        return self.is_admin
+        return self.is_superuser
 
     def is_superuser(self):
-        return self.is_admin
+        return self.is_superuser
 
 class Department(models.Model):
     """
     学校/校区/年级/班级
     """
     name = models.CharField(max_length=30, verbose_name="机构名称")
-    super_department = models.ForeignKey('self')
-
-class Campus(models.Model):
-    name = models.CharField(max_length=30, verbose_name="校区")
+    code = models.CharField(max_length=30, verbose_name="机构代码")
+    super_department = models.ForeignKey('self',on_delete=models.CASCADE)
 
 
 class Student(BaseUser):
@@ -90,13 +86,39 @@ class Student(BaseUser):
     学生模型
     """
     name = models.CharField(max_length=30, verbose_name="姓名")
+    eid = models.CharField(max_length=30, verbose_name="学籍号")
     sex = models.CharField(max_length=30, null=True, verbose_name="性别", blank=True)
-    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="学校",db_index=True)
-    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="校区",db_index=True)
+    school = models.CharField(max_length=30, null=True, blank=True, verbose_name="学校",db_index=True)
+    campus = models.CharField(max_length=30, null=True, blank=True, verbose_name="校区",db_index=True)
     grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="年级",db_index=True)
-    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="班级",db_index=True)
-    birthday = models.DateField(null=True, blank=True, verbose_name="生日")
+    student_class = models.CharField(max_length=30, null=True, blank=True, verbose_name="班级",db_index=True)
     province = models.CharField(max_length=30, null=True, blank=True, verbose_name="省份")
     city = models.CharField(max_length=30, null=True, blank=True, verbose_name="城市")
     district = models.CharField(max_length=30, null=True, blank=True, verbose_name="地区")
     created_at = models.DateTimeField(default=timezone.now, verbose_name='创建时间',db_index=True)
+
+class Teacher(BaseUser):
+    """
+    学生模型
+    """
+    name = models.CharField(max_length=30, verbose_name="姓名")
+    sex = models.CharField(max_length=30, null=True, verbose_name="性别", blank=True)
+    school = models.CharField(max_length=30, null=True, blank=True, verbose_name="学校",db_index=True)
+    campus = models.CharField(max_length=30, null=True, blank=True, verbose_name="校区",db_index=True)
+    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="年级",db_index=True)
+    study_class = models.CharField(max_length=30, null=True, blank=True, verbose_name="班级",db_index=True)
+    province = models.CharField(max_length=30, null=True, blank=True, verbose_name="省份")
+    city = models.CharField(max_length=30, null=True, blank=True, verbose_name="城市")
+    district = models.CharField(max_length=30, null=True, blank=True, verbose_name="地区")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='创建时间',db_index=True)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30, verbose_name="学科名称")
+
+class Course(models.Model):
+    """
+    课程-任课老师关联表
+    """
+    teacher = models.ForeignKey('Teacher',on_delete=models.CASCADE,verbose_name="任课老师",db_index=True)
+    subject = models.ForeignKey('Subject',on_delete=models.CASCADE,verbose_name="学科",db_index=True)
+    teach_class = models.CharField(max_length=30, verbose_name="班级",db_index=True)
