34d7e8618833978abeb79495872f96484494744d
xfhan <erazer_control@163.com>
2019-12-09 09:44:10 +0800

    1.is_superuser->is_admin 2.设置学生/教师的外键department

--- users/models.py
+++ users/models.py
@@ -31,14 +31,14 @@ class MyUserManager(BaseUserManager):
             phone,
             password=password,
         )
-        user.is_superuser = True
+        user.is_admin = True
         user.save(using=self._db)
         return user
 
 class BaseUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(verbose_name="身份证号",max_length=18, unique=True, primary_key=True)
     phone = models.CharField(verbose_name="手机号码", unique=True, max_length=15, null=True, blank=True)
-    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     objects = MyUserManager()
 
     USERNAME_FIELD = 'username'
@@ -67,10 +67,10 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
 
     @property
     def is_staff(self):
-        return self.is_superuser
+        return self.is_admin
 
     def is_superuser(self):
-        return self.is_superuser
+        return self.is_admin
 
 class Department(models.Model):
     """
@@ -88,37 +88,40 @@ class Student(BaseUser):
     name = models.CharField(max_length=30, verbose_name="姓名")
     eid = models.CharField(max_length=30, verbose_name="学籍号")
     sex = models.CharField(max_length=30, null=True, verbose_name="性别", blank=True)
-    school = models.CharField(max_length=30, null=True, blank=True, verbose_name="学校",db_index=True)
-    campus = models.CharField(max_length=30, null=True, blank=True, verbose_name="校区",db_index=True)
-    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="年级",db_index=True)
-    student_class = models.CharField(max_length=30, null=True, blank=True, verbose_name="班级",db_index=True)
+    school = models.ForeignKey('department', null=True, blank=True, verbose_name="学校",db_index=True,on_delete=models.CASCADE,related_name="school")
+    campus = models.ForeignKey('department', null=True, blank=True, verbose_name="校区",db_index=True,on_delete=models.CASCADE,related_name="campus")
+    grade = models.ForeignKey('department', null=True, blank=True, verbose_name="年级",db_index=True,on_delete=models.CASCADE,related_name="grade")
+    study_class = models.ForeignKey('department', null=True, blank=True, verbose_name="班级",db_index=True,on_delete=models.CASCADE,related_name="study_class")
     province = models.CharField(max_length=30, null=True, blank=True, verbose_name="省份")
     city = models.CharField(max_length=30, null=True, blank=True, verbose_name="城市")
     district = models.CharField(max_length=30, null=True, blank=True, verbose_name="地区")
     created_at = models.DateTimeField(default=timezone.now, verbose_name='创建时间',db_index=True)
-
+    def __str__(self):
+        return self.name
 class Teacher(BaseUser):
     """
     学生模型
     """
     name = models.CharField(max_length=30, verbose_name="姓名")
     sex = models.CharField(max_length=30, null=True, verbose_name="性别", blank=True)
-    school = models.CharField(max_length=30, null=True, blank=True, verbose_name="学校",db_index=True)
-    campus = models.CharField(max_length=30, null=True, blank=True, verbose_name="校区",db_index=True)
-    grade = models.CharField(max_length=30, null=True, blank=True, verbose_name="年级",db_index=True)
-    study_class = models.CharField(max_length=30, null=True, blank=True, verbose_name="班级",db_index=True)
+    school = models.ForeignKey('department', null=True, blank=True, verbose_name="学校",db_index=True,on_delete=models.CASCADE,related_name="teach_school")
+    campus = models.ForeignKey('department', null=True, blank=True, verbose_name="校区",db_index=True,on_delete=models.CASCADE,related_name="teach_campus")
+    grade = models.ForeignKey('department', null=True, blank=True, verbose_name="年级",db_index=True,on_delete=models.CASCADE,related_name="teach_grade")
+    manage_class = models.ForeignKey('department', null=True, blank=True, verbose_name="班级",db_index=True,on_delete=models.CASCADE,related_name="manage_class")
     province = models.CharField(max_length=30, null=True, blank=True, verbose_name="省份")
     city = models.CharField(max_length=30, null=True, blank=True, verbose_name="城市")
     district = models.CharField(max_length=30, null=True, blank=True, verbose_name="地区")
     created_at = models.DateTimeField(default=timezone.now, verbose_name='创建时间',db_index=True)
-
+    def __str__(self):
+        return self.name
 class Subject(models.Model):
     name = models.CharField(max_length=30, verbose_name="学科名称")
-
+    def __str__(self):
+        return self.name
 class Course(models.Model):
     """
     课程-任课老师关联表
     """
     teacher = models.ForeignKey('Teacher',on_delete=models.CASCADE,verbose_name="任课老师",db_index=True)
     subject = models.ForeignKey('Subject',on_delete=models.CASCADE,verbose_name="学科",db_index=True)
-    teach_class = models.CharField(max_length=30, verbose_name="班级",db_index=True)
+    teach_class = models.ForeignKey('department', verbose_name="授课班级",db_index=True,on_delete=models.CASCADE ,related_name="teach_class")
